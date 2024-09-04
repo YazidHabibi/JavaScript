8cdcf21f1696c2321ccb2d5314bf1aa2b91916b1
Yazid Habibi <yazid2505@student.abudzar.sch.id>
2024-09-04 10:42:08 +0700

    Update Latihan 10 function

--- JavaScript/meeting10/Latihan10.js
+++ JavaScript/meeting10/Latihan10.js
@@ -1,84 +1,91 @@
 // Soal Gambar nomor 1
-function registerUser(name, userType) {
-    if (userType === "VIP") {
-      return `Welcome VIP ${name}`;
-    } else {
-      return `Welcome ${name}`;
-    }
+function registerUser(nama, usertype) {
+  if (usertype === 'VIP') {
+    return ` Welcome VIP ${nama}!`
+  }else {
+    return  ` Welcome ${nama}!`
   }
-  
-  console.log(registerUser("Nina", "VIP"));
-  console.log(registerUser("Nina", "NON VIP"));
-  
-  function applyDiscount(userType, purchaseAmount) {
-    if (userType === "VIP") {
-      var discount = 0.2;
-    } else {
-      var discount = 0.1;
-    }
-  
-    return purchaseAmount - purchaseAmount * discount;
+}
+console.log(registerUser('Nina', 'VIP',));
+console.log(registerUser('Nina','NON VIP'));
+
+function applyDiscount(usertype, purchaseAmount) {
+  let diskon = 0;
+  if (usertype === 'VIP') {
+      diskon = 20;
+  }else {
+    diskon = 10;
   }
-  
-  console.log(applyDiscount("VIP", 200));
-  console.log(applyDiscount("NON VIP", 200));
-  
-  // Soal Gambar nomor 2
-  function calculatePrice(quantity, pricePerItem, isMember) {
-    if (isMember === true) {
-      return quantity * pricePerItem - quantity * pricePerItem * 0.15;
-    } else {
-      return quantity * pricePerItem;
-    }
+  return purchaseAmount - (purchaseAmount * diskon /100 )
+
+}
+
+console.log(applyDiscount('VIP',200 ));
+console.log(applyDiscount('NON VIP',100));
+
+// Tugas gambar nomor 2
+
+function calculatePrice (quantity,pricePeritem, Ismember) {
+  total = quantity * pricePeritem
+  if (Ismember === true) {
+    return total - (total * 15/100)
+  }else {
+    return total
   }
-  
-  console.log(calculatePrice(3, 150, true));
-  console.log(calculatePrice(3, 150, false));
-  
-  function confirmPurchase(isConfirmed, totalPrice) {
-    if (isConfirmed === true) {
-      return `Purchase Confirmed With Total Price ${totalPrice}.`;
-    } else {
-      return `Purchase Not Confirmed`;
-    }
+}
+console.log(calculatePrice(3,150, true));
+
+function confirmPurchase (isConfirmed, totalPrice) {
+  if (isConfirmed === true) {
+    return  `Purchase Is Confirmed With Total Price : ${totalPrice}`
+  } else {
+    return ` Purchase Not Confirmed`
   }
-  
-  console.log(confirmPurchase(true, 1000));
-  console.log(confirmPurchase(false, 1000));
-  
-  // Soal 3 GDrive
-  function checkStock(requestedQuantity, item) {
-    var Stock = 50;
-  
-    if (requestedQuantity > Stock) {
-      return `Stock insufficient for ${item}.`;
-    } else {
-      return `Stock sufficient for ${item}.`;
-    }
+}
+console.log(confirmPurchase(true, calculatePrice(3, 150,true)));
+
+// soal gambar nomor 3
+
+function checkStock (item,requestedQuantity) {
+  let stockbarang = 50
+  if (requestedQuantity > stockbarang) {
+    return `Stock insufficient for ${item}`
+  }else {
+    return `Stock sufficient for ${item}`
+  } 
+}
+console.log(checkStock("Laptop", 60));
+
+function  restockItem (item,additionalStock) {
+ let stockBarang = 5 + additionalStock
+ let requestedQuantity = 60;
+  if (requestedQuantity <= stockBarang) {
+      return `Restock complete for ${item}`
+  } else {
+    return `Restock Needed for ${item}`
   }
-  
-  console.log(checkStock(60, "Laptop"));
-  console.log(checkStock(50, "Laptop"));
-  
-  // Soal 4 GDrive
-  function calculateTax(price, location) {
-    if (location == "Jakarta") {
-      return price * 0.1;
-    }
-    if (location == "Bandung") {
-      return price * 0.05;
-    }
+}
+console.log(restockItem('Laptop',55));
+
+// Latihan gambar nomor 4
+
+function calculateTax (price,location) {
+  pajak = 0
+  if (location === 'Jakarta') {
+    pajak = 10
+  }else if (location === 'Bandung') {
+    pajak = 5
   }
-  
-  var finalPrice = calculateTax(10000, "Jakarta");
-  console.log(finalPrice);
-  
-  function displayResult(finalPrice, paymentStatus) {
-    if (paymentStatus == "paid") {
-      return `Payment successful! Total amount:` + finalPrice;
-    } else {
-      return `Payment pending`;
-    }
+  return price - (price * pajak / 100)
+}
+console.log(calculateTax(1000,'Jakarta'));
+
+function displayResult (finalPrice,paymentStatus) {
+  if (paymentStatus === 'paid') {
+    return `Payment successful Total amount : ${finalPrice}`
+  }else {
+    return  `Payment Pending`
   }
-  
-  console.log(displayResult(finalPrice, "paid"));
+}
+console.log(displayResult(calculateTax(1000,'Jakarta'),'paid'));
+
