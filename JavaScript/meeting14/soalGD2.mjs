b5fd6fdf62da63bedddcc38edc53454719c86a2a
Yazid Habibi <yazid2505@student.abudzar.sch.id>
2024-11-22 10:17:23 +0700

    Update14

---
+++ JavaScript/meeting14/soalGD2.mjs
@@ -0,0 +1,23 @@
+let a = 10;
+
+let b = 2;
+
+async function calculateDivision() {
+  if (b > 0) {
+    return a / b;
+  } else {
+    throw "Cannot divide by zero!";
+  }
+}
+
+const runCalculation = async () => {
+  try {
+    const result = await calculateDivision();
+
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+runCalculation();
